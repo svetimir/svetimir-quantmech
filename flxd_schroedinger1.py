406e4fad13ed4b8fc629101ddfb6db378f1bec50
svetimir <ifi@yandex.ru>
2018-09-07 11:19:27 +0300

    0.1, minor

--- flxd_schroedinger1.py
+++ flxd_schroedinger1.py
@@ -10,17 +10,34 @@
 
 from scipy import integrate
 import numpy as np
+# на будущее
 import math
+# ...
 import matplotlib.pyplot as plt
 
+# энергия частицы
 e=1.3
+# константа частицы
 h=0.5
+# фактор потенциальной ямы
 A=1.5
+# константы интегрирования
+a = 0.0
+b = 10.0
+step = 0.005
+# начаьлные условия
+alpha=0
+beta=1
+# неиспользуемые дополнительные переменные
 omega=1
 phi=0
 
 
 def u(x):
+    """
+    Функция потенциала,
+    прямоугольная потенциальная яма
+    """
     if x>5.0:
         return A*1e1
     else:
@@ -28,18 +45,20 @@ def u(x):
 
 
 def se_solve(Y,x):
+    """
+    Функция, задающая систему дифференциальных уравнений
+    """
     return [Y[1],(e-u(x))/h*Y[0]]
 
 
 def main():
-    a=0.0
-    b=10.0
-    step=0.005
-    N=int((b-a)/step)
+    # массивы для решения задачи
     a_x=np.arange(a,b,step)
     y=np.arange(a,b,step)
-    asol=integrate.odeint(se_solve,[0,1],a_x)
-    print(asol)
+    # интегрируем систему уравнений
+    asol=integrate.odeint(se_solve,[alpha,beta],a_x)
+    # на графике будем изображать квадрат модуля волновой функции
+    # несущий физический смысл плотности вероятности
     for i in range(0,len(asol)):
         y[i]=asol[i][0]**2
     plt.plot(y)
